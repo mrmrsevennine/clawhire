bf53da7cd7f9393cf2f72f115331190f8226846a
Tim <tim@bike-doctor-freiburg.de>
2026-02-07 01:29:26 +0100

    feat: rename token to $CLAWHIRE - 51/51 tests, redeployed

--- skill/usdc-agent-tasks/contracts/ClawToken.sol
+++ skill/usdc-agent-tasks/contracts/ClawToken.sol
@@ -5,15 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title ClawToken ($CLAW)
- * @notice Governance and revenue-sharing token for the clawhire protocol.
+ * @title clawhire ($CLAWHIRE)
+ * @notice Revenue-sharing token for the clawhire protocol.
  *         Fixed supply of 100,000,000 tokens minted at deployment.
  *         Holders can stake in RevenueShare contract to earn USDC from platform fees.
  */
 contract ClawToken is ERC20, Ownable {
     uint256 public constant MAX_SUPPLY = 100_000_000 * 1e18; // 100M tokens
 
-    constructor(address initialHolder) ERC20("Claw Token", "CLAW") Ownable(msg.sender) {
+    constructor(address initialHolder) ERC20("clawhire", "CLAWHIRE") Ownable(msg.sender) {
         require(initialHolder != address(0), "Invalid initial holder");
         _mint(initialHolder, MAX_SUPPLY);
     }
